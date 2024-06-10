b3111e1c921b759c9735ee082e8a44e79cdd2043
Ioannis Sifnaios <john_sif@hotmail.com>
2024-06-10 23:01:51 +0200

    undo change

--- conf.py
+++ conf.py
@@ -66,6 +66,7 @@ html_sidebars = {
             #'postcard.html', 'navigation.html',
             'recentposts.html', #'tagcloud.html',
             #'categories.html',  'archives.html',
+            #'searchbox.html',
             ],
     }
 
